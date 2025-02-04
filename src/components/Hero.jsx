d44d8c940a2856b8ea6d88ca54dd5f43234518f4
Kale-kode <kaileliu2017@gmail.com>
2025-02-04 12:26:43 +0000

    new gifs + fixed mini video animation + created about section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,15 @@ import Button from './Button'
 import { TiLocationArrow } from 'react-icons/ti'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
   // STATES
   const [currentIndex, setCurrentIndex] = useState(1)
+  const [prevIndex, setPrevIndex] = useState(1)
+  const [videoAnimating, setVideoAnimating] = useState(false)
   const [hasClicked, setHasClicked] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   const [loadedVideos, setLoadedVideos] = useState(0)
@@ -27,7 +32,7 @@ const Hero = () => {
   }
 
   // the useGSAP hook triggers based on the dependencies value in the second argument
-  // in this case animate mini video player when currentIndex changes i.e. on click
+  // animate mini video player when currentIndex changes i.e. on click
   useGSAP(() => {
     if (hasClicked) {
       // make next video visible
@@ -43,7 +48,14 @@ const Hero = () => {
         duration: 1,
         ease: 'power1.inOut',
         // make the next video begin playing right away, even while animating
-        onStart: () => nextVideoRef.current.play(),
+        onStart: () => {
+          setVideoAnimating(true)
+          nextVideoRef.current.play()
+        },
+        onComplete: () => {
+          setVideoAnimating(false)
+          setPrevIndex(prevIndex => prevIndex % totalVideos + 1)
+        },
       })
       // make the mini video frame animate to grow in
       gsap.from('#current-video', {
@@ -55,8 +67,30 @@ const Hero = () => {
     }
   }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+
+  // scroll animation for hero video frame
+  useGSAP(() => {
+    // apply trapzoid clip path to the video frame
+    gsap.set('#video-frame', { 
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius: '0 0 40% 12%',
+    })
+    // animate on scroll starting from a full clip path polygon
+    gsap.from(('#video-frame'), {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      }
+    })
+  })
+
   // retrieve a hero video from a given index
-  const getVideoSrc = (index) => `videos/hero-${index}.mp4`
+  const getVideoSrc = (index) => `videos/hero-${index}.gif`
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
@@ -65,8 +99,8 @@ const Hero = () => {
           <div>
 
             <div className='mask-clip-path absolute-center absolute z-50 cursor-pointer overflow-hidden rounded-lg'>
-              <div onClick={handleMiniVideoClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in-out hover:scale-100 hover:opacity-100'>
-                <video 
+              <div onClick={handleMiniVideoClick} className={`origin-center scale-50 opacity-0 transition-all duration-500 ease-in-out hover:scale-100 hover:opacity-100 ${videoAnimating ? 'pointer-events-none' : 'pointer-events-auto'}`}>
+                <img 
                 ref={nextVideoRef} 
                 src={getVideoSrc(currentIndex%totalVideos+1)}
                 loop
@@ -78,7 +112,7 @@ const Hero = () => {
               </div>
             </div>
 
-            <video 
+            <img 
             ref={nextVideoRef}
             src={getVideoSrc(currentIndex)}
             loop
@@ -88,29 +122,29 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
             />
 
-            <video 
-            src={getVideoSrc(currentIndex)}
+            <img 
+            src={getVideoSrc(prevIndex)}
             autoPlay
             loop
             muted
-            className='absolute top-0 left-0 size-full object-cover object-center border border-transparent'
+            className='absolute top-0 left-0 size-full object-cover object-center'
             onLoadedData={handleVideoLoad}
             />
 
           </div>
 
-          <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>IDE<b>A</b>S</h1>
+          <h1 className='special-font hero-heading absolute bottom-10 right-10 z-40 text-blue-75'>LI<b>U</b></h1>
           <div className='absolute top-0 left-0 z-40 size-full'>
             <div className='mt-24 px-5 sm:px-10'>
-              <h1 className='special-font hero-heading text-blue-75'>BUILDI<b>N</b>G</h1>
-              <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>Bringing ideas to life through code.<br />Pushing the limits of the web.</p>
+              <h1 className='special-font hero-heading text-blue-75'>K<b>A</b>ILE</h1>
+              <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>Front end developer and designer.<br />Pushing the limits of the web.</p>
               <Button id="explore-projects" text="Explore Projects" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
             </div>
           </div>
 
         </div>
 
-        <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>IDE<b>A</b>S</h1>
+        <h1 className='special-font hero-heading absolute bottom-10 right-10 text-black'>LI<b>U</b></h1>
 
     </div>
   )
